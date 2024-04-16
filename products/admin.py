fbbbf79b339be80b9d108af57f177f868b323473
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-04-16 07:13:29 +0100

    Update: organize products in admin

--- products/admin.py
+++ products/admin.py
@@ -1,6 +1,34 @@
 from django.contrib import admin
 from .models import Product, Collection, Variant
 
+class ProductAdmin(admin.ModelAdmin):
+	list_display = (
+		'sku',
+		'title',
+		'collection',
+		'price',
+		'stock',
+		'image',
+	)
+
+class ProductAdmin(admin.ModelAdmin):
+	list_display = (
+		'title',
+		'price',
+		'stock'
+		'image',
+	)
+
+	ordering = ('title',)
+
+class CollectionAdmin(admin.ModelAdmin):
+	list_display = (
+		'user_friendly_name',
+		'name',
+	)
+
+	ordering = ('user_friendly_name',)
+
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Collection)
