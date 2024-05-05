f1bca54bde195a4a2151717a472e9fe6ea8cf46b
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-05-05 21:20:47 +0100

    Add: order history list to profile, and order summary preview from profile

--- profiles/views.py
+++ profiles/views.py
@@ -3,6 +3,7 @@ from django.contrib import messages
 
 from .models import UserProfile
 from .forms import UserProfileForm
+from checkout.models import Order
 
 def profile(request):
 	""" Display user's profile """
@@ -13,7 +14,7 @@ def profile(request):
 
 	if request.method == 'POST':
 		form = UserProfileForm(request.POST, instance=profile)
-		if form.valud():
+		if form.is_valid():
 			form.save()
 			messages.success(request, 'profile updated successfully.')
 
@@ -23,4 +24,20 @@ def profile(request):
 		'orders' : orders,
 	}
 	
+	return render(request, template, context)
+
+def order_history(request, order_number):
+	order = get_object_or_404(Order, order_number=order_number)
+
+	messages.info(request, (
+		f'This is a past confirmation for order {order.order_number}.'
+		'A confirmation email was sent on the date of the purchase.'
+	))
+
+	template = 'checkout/checkout_success.html'
+	context = {
+		'order': order,
+		'from_profile' : True,
+	}
+
 	return render(request, template, context)
