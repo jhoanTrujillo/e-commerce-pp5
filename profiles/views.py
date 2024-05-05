b6b0cad264c5bca7b954f47e11cb048f1d3a0fff
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-05-05 19:20:20 +0100

    Add: profile model, default address form, views, and template

--- profiles/views.py
+++ profiles/views.py
@@ -1,8 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+
+from .models import UserProfile
+from .forms import UserProfileForm
 
 def profile(request):
 	""" Display user's profile """
+	profile = get_object_or_404(UserProfile, user=request.user)
+	
+	form = UserProfileForm(instance=profile)
+	orders = profile.orders.all()
+
+	if request.method == 'POST':
+		form = UserProfileForm(request.POST, instance=profile)
+		if form.valud():
+			form.save()
+			messages.success(request, 'profile updated successfully.')
+
 	template = 'profiles/profile.html'
-	context = {}
+	context = {
+		'form' : form,
+		'orders' : orders,
+	}
 	
 	return render(request, template, context)
