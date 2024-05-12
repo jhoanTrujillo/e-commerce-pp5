6d7613b86c2d0ec2a14f9fc2a39443b2d1d7a757
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-05-12 11:05:15 +0100

    Update: all products view to display variants in collection page

--- products/views.py
+++ products/views.py
@@ -1,41 +1,42 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
-from django.db.models import Q
+from django.db.models import Q, Prefetch
 from .models import Product, Collection, Variant
 from .forms import ProductForm, VariantForm
 
-
 def all_products(request):
-	"""
-	Show all products with sorting and search query.
-	"""
-	products = Product.objects.all()
-	query = None
-	collection = None
+    """
+    Show all products with sorting and search query.
+    """
+    products = Product.objects.all().prefetch_related(
+        Prefetch('variant_set', queryset=Variant.objects.select_related('product'))
+    )
+    query = None
+    collection = None
 
-	# Checks for a get request
-	if request.GET:
-		if 'collection' in request.GET:
-			collections = request.GET['collection'].split(',')
-			products = products.filter(collection__name__in=collections)
-			collections = Collection.objects.filter(name__in=collections)
+    # Checks for a get request
+    if request.GET:
+        if 'collection' in request.GET:
+            collections = request.GET['collection'].split(',')
+            products = products.filter(collection__name__in=collections)
+            collections = Collection.objects.filter(name__in=collections)
 
-		if 'q' in request.GET:
-			query = request.GET['q']
-			if not query:
-				messages.error(request, "You didn't enter any search criteria")
-				return redirect(reverse('products'))
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria")
+                return redirect(reverse('products'))
 
-			queries = Q(title__icontains=query) | Q(description__icontains=query)
-			products = products.filter(queries)
+            queries = Q(title__icontains=query) | Q(description__icontains=query)
+            products = products.filter(queries)
 
-	context = {
-		'products': products,
-		'search_term': query,
-		'current_collections': collection,
-	}
+    context = {
+        'products': products,
+        'search_term': query,
+        'current_collections': collection,
+    }
 
-	return render(request, "products/products.html", context)
+    return render(request, "products/products.html", context)
 
 def product_details(request, product_id):
 	"""
@@ -106,7 +107,7 @@ def add_product_or_variant(request):
 
 def edit_product_or_variant(request, item_id):
 	""" Edit a product or variant in the store """
-	is_variant = request.GET.get('is_variant', False)
+	is_variant = request.GET.get('is-variant', False)
 
 	if is_variant:
 		product_or_variant = get_object_or_404(Variant, pk=item_id)
