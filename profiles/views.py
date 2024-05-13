fe4249b6ace092b5d834045ca7f76753dd64d1c2
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-05-13 18:49:28 +0100

    Update: cosmetic changes to the profile.html

--- profiles/views.py
+++ profiles/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, get_object_or_404
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 
 from .models import UserProfile
 from .forms import UserProfileForm
 from checkout.models import Order
 
+@login_required
 def profile(request):
 	""" Display user's profile """
 	profile = get_object_or_404(UserProfile, user=request.user)
@@ -26,17 +28,22 @@ def profile(request):
 	
 	return render(request, template, context)
 
+@login_required
 def order_history(request, order_number):
 	order = get_object_or_404(Order, order_number=order_number)
+	product_line_items = list(order.productlineitem.all())
+	variant_line_items = list(order.variantlineitem.all())
 
 	messages.info(request, (
 		f'This is a past confirmation for order {order.order_number}.'
 		'A confirmation email was sent on the date of the purchase.'
 	))
-
+	
 	template = 'checkout/checkout_success.html'
 	context = {
 		'order': order,
+		'product_line_items' :product_line_items,
+		'variant_line_items' : variant_line_items,
 		'from_profile' : True,
 	}
 
