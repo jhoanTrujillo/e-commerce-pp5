ae1b8d0d35489a1b5a642b1ed80072d5f53543f2
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-05-13 18:47:35 +0100

    Add: css fields and general js code for store functionality

--- static/js/base.js
+++ static/js/base.js
@@ -14,6 +14,60 @@ class Message extends HTMLElement {
 	  this.remove();
 	}
   }
-  
-  // Define the custom element
-  window.customElements.define('message-box', Message);
+
+// Debounce form submittion to prevent multiple submittions
+const setupFormSubmitListener = () => {
+    const forms = document.querySelectorAll('form');
+
+    forms.forEach(parentForm => {
+        const submitButton = parentForm.querySelector('button[type="submit"]');
+        const formInputs = parentForm.querySelectorAll('input, select, textarea');
+
+        parentForm.addEventListener('submit', (event) => {
+            event.preventDefault();
+
+            // Add is-skeleton class to the submit button
+            submitButton.classList.add('is-skeleton');
+
+            // Add is-skeleton class to all inputs in the form
+            formInputs.forEach(input => {
+                input.classList.add('is-skeleton');
+            });
+
+            // Submit the form after a brief delay
+            setTimeout(() => {
+                parentForm.submit();
+            }, 500);
+        });
+    });
+};
+
+// Navbar
+
+function setupNavbarBurger() {
+    // Get all "navbar-burger" elements
+    const $navbarBurgers = Array.prototype.slice.call(document.querySelectorAll('.navbar-burger'), 0);
+
+    // Add a click event on each of them
+    $navbarBurgers.forEach(el => {
+        el.addEventListener('click', () => {
+            // Get the target from the "data-target" attribute
+            const target = el.dataset.target;
+            const $target = document.getElementById(target);
+
+            // Toggle the "is-active" class on both the "navbar-burger" and the "navbar-menu"
+            el.classList.toggle('is-active');
+            $target.classList.toggle('is-active');
+        });
+    });
+}
+
+// Define class elements
+window.customElements.define('message-box', Message);
+
+// Call the functions when the DOM content is loaded
+document.addEventListener('DOMContentLoaded', () => {
+    setupNavbarBurger();
+	setupFormSubmitListener();
+});
+
