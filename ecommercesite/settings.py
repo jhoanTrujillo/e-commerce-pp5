03251e808a28e42f742afdee8baa86dc22ef0eb2
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-05-14 21:28:36 +0100

    Update: settings to allow for email services

--- ecommercesite/settings.py
+++ ecommercesite/settings.py
@@ -147,11 +147,11 @@ STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-STORAGES = {
-    "staticfiles": {
-        "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
-    },
-}
+# STORAGES = {
+#     "staticfiles": {
+#         "BACKEND": "whitenoise.storage.CompressedManifestStaticFilesStorage",
+#     },
+# }
 
 if 'USE_AWS' in os.environ:
     # Cache control
@@ -159,6 +159,7 @@ if 'USE_AWS' in os.environ:
         'Expires' : 'Thu, 31 Dec 2099 20:00:00 GMT',
         'CacheControl' : 'max-age=94608000'
     }
+    
     # Bucket Config
     AWS_STORAGE_BUCKET_NAME = 'ci-project-5-joe-pins-be851091e775'
     AWS_S3_REGION_NAME = 'eu-north-1'
@@ -185,8 +186,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # Needed by 'django.contrib.sites'
 SITE_ID=1
 
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-
 # Details for AllAuth authentication
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email'
 ACCOUNT_EMAIL_REQUIRED = True
@@ -205,4 +204,14 @@ STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
-SITE_ID = 1
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'joecrazypins@example.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    DEFAULT_FROM_EMAIL = os.environ.get('EMAIL_HOST_USER')
