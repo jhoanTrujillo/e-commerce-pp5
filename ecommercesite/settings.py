65e7880124c5a49846340376b497d8b1b7cb993b
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-05-14 19:37:26 +0100

    Fix: changing settings to allow for static file collection

--- ecommercesite/settings.py
+++ ecommercesite/settings.py
@@ -9,15 +9,13 @@ load_dotenv(find_dotenv()) # take environment variables from .env
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
-ALLAUTH_DIR = os.path.join(BASE_DIR, 'templates', 'allauth')
-INCLUDES_DIR = os.path.join(BASE_DIR, 'includes')
-
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'django-insecure-k$vu6x2bqzf($rbwl*o7om!0yu6g*r=m84d%h(v0pkh(vbxg$b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.environ.get('DEBUG', False)
+DEBUG = os.environ.get('DEBUG')
+
 ALLOWED_HOSTS = ['ci-project-5-joe-pins-be851091e775.herokuapp.com','.herokuapp.com','localhost','127.0.0.1']
 
 # Application definition
@@ -29,18 +27,16 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 	'django.contrib.sites',
-    # Project apps
+	'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
 	'home',
 	'products',
 	'cart',
 	'checkout',
 	'profiles',
 	
-	# Third-party apps
-	'allauth',
-    'allauth.account',
-    'allauth.socialaccount',
-    'allauth.socialaccount.providers.google',
+	# Third-party apps
 	'crispy_forms',
     'crispy_bulma',
     'storages',
@@ -49,25 +45,27 @@ INSTALLED_APPS = [
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    "whitenoise.middleware.WhiteNoiseMiddleware",
     # Allauth account middleware:
     "allauth.account.middleware.AccountMiddleware",
 ]
 
 ROOT_URLCONF = 'ecommercesite.urls'
 
+CRISPY_ALLOWED_TEMPLATE_PACKS = ("bulma",)
+CRISPY_TEMPLATE_PACK = "bulma"
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
         'DIRS': [
-            TEMPLATES_DIR,
-            ALLAUTH_DIR,
-            INCLUDES_DIR,
+            os.path.join(BASE_DIR, "templates"),
+            os.path.join(BASE_DIR, "templates", "allauth")
         ],
         'APP_DIRS': True,
         'OPTIONS': {
@@ -141,7 +139,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
 
@@ -188,11 +187,10 @@ LOGIN_REDIRECT_URL = '/'
 FREE_DELIVERY_THRESHOLD = 30
 STANDARD_DELIVERY_PERCENTAGE = 10
 
-CRISPY_ALLOWED_TEMPLATE_PACKS = ("bulma",)
-CRISPY_TEMPLATE_PACK = "bulma"
-
 # Stripe
-STRIPE_CURRENCY = 'usd'
+STRIPE_CURRENCY = 'eur'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
+
+SITE_ID = 1
