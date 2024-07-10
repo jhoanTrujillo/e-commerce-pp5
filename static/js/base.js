6f02ba6739d43284b6bb8b141be417481470248a
jhoanTrujillo <jhoantrujillo95@gmail.com>
2024-07-10 08:31:12 +0100

    Fix: bug with the toast not closing

--- static/js/base.js
+++ static/js/base.js
@@ -1,36 +1,20 @@
-(() => {
-class MessageBox extends HTMLElement {
-    constructor() {
-        super();
-    }
+/**
+ * Function to close the toast message box.
+ * Returns no values.
+ */
+const toast = () => {
+    const toastElement = document.getElementById("message-box");
+    const closeToastButton = toastElement.querySelector('[aria-label="delete"]');
 
-    connectedCallback() {
-        // Add event listener to handle click on delete button
-        this.querySelector('button[aria-label="delete"]').addEventListener('click', this.deleteMessage.bind(this));
-        this.style.width = '20rem';
-
-        // Schedule fade-out and deletion after one second
-        setTimeout(() => {
-            this.fadeOutAndDelete();
-        }, 2000);
-    }
-
-    fadeOutAndDelete() {
-        // Add 'fade-out' class to trigger fade-out animation
-        this.classList.add('fade-out');
-
-        // Listen for 'animationend' event to remove the element after animation completes
-        this.addEventListener('animationend', () => {
-            this.deleteMessage();
-        });
-    }
-
-    deleteMessage() {
+    const closeToast = () => {
         // Remove the message element from the DOM
-        this.remove();
+        toastElement.remove();
     }
+
+    closeToastButton.addEventListener("click", () => {
+        closeToast();
+    });
+
 }
 
-// Define class elements
-window.customElements.define('message-box', MessageBox);
-})()
+document.addEventListener("DOMContentLoaded", toast);
